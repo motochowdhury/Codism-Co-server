6bab0a81ac10d47262e6b9eced39f24641699f06
Moto Chowdhury <motochowdhury@gmail.com>
2022-10-26 19:35:14 +0600

    server setup done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(
+    "Hay if You're finding (codism-co) server, Then you're in Right place"
+  );
+});
+
+app.listen(5000, () => console.log("YAAY!! server is running at 5000"));
